3b958fc7f1e56bfa4f4902e67ac09d82f5fd976b
Nandin <Machadofernando986@gmail.com>
2026-05-01 16:13:01 -0300

    Realizei o JS hoje, mas ele ficou com alguns bugs, devo corrigir em breve, também essa semana fiz uma pequena correção no css

---
+++ scirpt.js
@@ -0,0 +1,8 @@
+
+function recebermsg() {
+let msg = document.getElementById("mensagem")
+let name  = document.getElementById("nome")
+
+msg.innerHTML = "Olá, " + name.value + " prazer em conhece-lo"
+
+}
